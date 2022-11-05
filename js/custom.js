3e112db0d7fece3f7b3424fad6510e0d221d288c
HiPeach <hi@hipeach.eu.org>
2022-11-05 13:37:44 +0800

    Site updated: 2022-11-05 13:37:44

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,9 @@
-// 即刻短文 参见 https://blog.zhheo.com/p/557c9e72.html
 if (document.querySelector('#bber-talk')) {
     var swiper = new Swiper('.swiper-container', {
       direction: 'vertical', // 垂直切换选项
       loop: true,
       autoplay: {
-      delay: 3000,
+      delay: 2500,
       pauseOnMouseEnter: true
     },
     });
